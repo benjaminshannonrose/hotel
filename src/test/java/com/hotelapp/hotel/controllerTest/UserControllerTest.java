a8d21ad44b17b59e7100a3ed30527938114d5791
Rose <brose@icct.com>
2017-10-26 16:27:09 -0400

    added unit tests for controller methods

---
+++ src/test/java/com/hotelapp/hotel/controllerTest/UserControllerTest.java
@@ -0,0 +1,4 @@
+package com.hotelapp.hotel.controllerTest;
+
+public class UserControllerTest {
+}
--- src/test/java/com/hotelapp/hotel/cucumberTest/WebLayerTest.java
+++
@@ -1,9 +0,0 @@
-package com.hotelapp.hotel.cucumberTest;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.web.WebAppConfiguration;
-
-@SpringBootTest
-@WebAppConfiguration
-public class WebLayerTest {
-}
